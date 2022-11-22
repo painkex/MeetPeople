352582a864cdb32b73c2f7743938c26501d7e2b0
joel irmiahu <joelirmiahu66@gmail.com>
2022-11-22 14:22:18 +0100

    test

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -3,6 +3,6 @@ class Offer < ApplicationRecord
   has_many :bookings
   validates :price_day, :description, :start_date, :end_date, :language, :address, presence: true
   validates :language, inclusion: { in: %w(Ruby Front Rails) }
-  validates :start_date, comparison: { greater_than: :end_date }
+  validates :end_date, comparison: { greater_than: :start_date }
   # validates :price_day, numeracality: { greater_than_or_equal_to: 0 }
 end
