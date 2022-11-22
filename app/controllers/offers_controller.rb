a537c1ef92d2bbb241a8732a7446808d6b722ba3
joel irmiahu <joelirmiahu66@gmail.com>
2022-11-22 15:39:28 +0100

    feur

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,42 +1,44 @@
 class OffersController < ApplicationController
   def index
-    @offers = Offer.all
+    @offers = policy_scope(Offer)
   end
 
   def show
-    authorize @offer
     @offer = Offer.find(params[:id])
     @bookings = @offer.bookings
+    authorize @offer
   end
 
   def edit
-    authorize @offer
     @offer = Offer.find(params[:id])
+    authorize @offer
   end
 
   def update
-    authorize @offer
     @offer = Offer.find(params[:id])
     @offer.update(offer_params)
     redirect_to offer_path(@offer)
+    authorize @offer
   end
 
   def new
-    authorize @offer
     @offer = Offer.new
+    authorize @offer
   end
 
   def create
-    authorize @offer
     @offer = Offer.new(offer_params)
     @offer.user = current_user
     @offer.save
     redirect_to offer_path(@offer)
+    authorize @offer
   end
 
   def destroy
+    @offer = Offer.find(params[:id])
+    @offer.destroy
+    redirect_to offers_path(@offer), status: :see_other
     authorize @offer
-    
   end
 
   private
